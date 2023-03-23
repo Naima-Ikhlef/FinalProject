d2cd54e67fe3d528a13755ba05cde401d6b69850
Naima-Ikhlef <113034327+Naima-Ikhlef@users.noreply.github.com>
2023-03-23 14:58:35 +0100

    Update gestionTemps.js

--- scripts/Utilitaire/gestionTemps.js
+++ scripts/Utilitaire/gestionTemps.js
@@ -1,1 +1,15 @@
+const joursSemaine = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche'];
 
+let ajd = new Date ();
+let options = {weekday: 'long'};
+let jourActuel = ajd.toLocaleDateString('fr-FR', options);
+
+//console.log(jourActuel, ajd);
+
+jourActuel = jourActuel.charAt(0).toUpperCase() + jourActuel.slice(1);
+
+let tabJoursOrdre = joursSemaine.slice(joursSemaine.indexOf(jourActuel)).concat(joursSemaine.slice(0, joursSemaine.indexOf(joursSemaine)));
+
+//console.log(tabJoursOrdre)
+
+export default tabJoursOrdre;
